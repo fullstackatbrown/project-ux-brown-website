f47637bd2b76a97baa48241ddfcb34d0b3044479
Aubrey-Li <63608597+Aubrey-Li@users.noreply.github.com>
2021-06-17 17:06:26 -0700

    fix about

--- website/src/footer.js
+++ website/src/footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
   render() {
     return (
       <div className='footer'>
-        <span className="footer-style">&#169;UX@Brown-RISD 2021</span>
+        <span className="footer-style">&#169;Design@Brown 2021</span>
       </div>
   );
   }
