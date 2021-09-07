668ba9ef7cef59b78cb34f80b77c8a7a67c057ea
Aubrey-Li <63608597+Aubrey-Li@users.noreply.github.com>
2021-09-07 16:52:46 -0400

    Router auto scroll to top

--- website/src/App.js
+++ website/src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import Footer from './header-footer/footer.js';
-import { Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route } from 'react-router-dom';
 import About from './about/about.js';
 import Home from './home/home.js';
 import Projects from './projects/projects.js';
 import Events from './events/events.js';
+import ScrollToTop from './ScrollToTop';
 
 function App() {
   return (
     <div>
+      <ScrollToTop />
       <Switch>
-        <Route exact path="/" component={Home}/>
+        <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
-        <Route path="/projects" component={Projects}/>
-        <Route path="/events" component={Events}/>
+        <Route path="/projects" component={Projects} />
+        <Route path="/events" component={Events} />
       </Switch>
       <Footer />
     </div>
